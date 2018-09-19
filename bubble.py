7f46c8037445f8f004fe8a32949110fbbc476114
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-19 07:55:17 -0500

    Function takes in array, stores length in variable, and iterates through array

--- bubble.py
+++ bubble.py
@@ -1,1 +1,11 @@
 print("Bubble Sort!")
+
+# Define function that takes in array and outputs sorted array
+def bubble_sort(array):
+	# Store length of array
+	arr_len = len(array)
+	for i in range(0, arr_len):
+		print(array[i])
+
+
+bubble_sort([3, 1, 0, 94, 32, 12, 13, 0, 4, 6, 0, 1])		
