9241baa7df8c680b3d6f19c15a51dd53f55e3989
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-19 08:01:39 -0500

    Function swaps element with elements less than it, effectively bubbling it up

--- bubble.py
+++ bubble.py
@@ -2,10 +2,26 @@ print("Bubble Sort!")
 
 # Define function that takes in array and outputs sorted array
 def bubble_sort(array):
+
 	# Store length of array
 	arr_len = len(array)
+
+	# Iterate through array
 	for i in range(0, arr_len):
-		print(array[i])
+
+		# Compare each element to all elements succeeding it
+		for j in range(1, arr_len):
+
+			# Swap it with elements that are less than it
+			if (array[j-1] > array[j]):
+
+				temp = array[j-1]
+
+				array[j-1] = array[j]
+
+				array[j] = temp
+
+
 
 
 bubble_sort([3, 1, 0, 94, 32, 12, 13, 0, 4, 6, 0, 1])		
